f75f8cf7f0e6294da70e80d4c14c78445e0fcad4
Joshin-Shionoiri <165250803+Joshin-Shionoiri@users.noreply.github.com>
2026-07-23 11:38:04 +0900

    Update firebase.js

--- in-ex/firebase.js
+++ in-ex/firebase.js
@@ -1,4 +1,4 @@
-// Firebase SDK 読み込み
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
